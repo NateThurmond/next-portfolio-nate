c0f3ee206f0891d33b606815278f016b507215b1
NateThurmond <nathanthurmond@yahoo.com>
2025-03-20 00:44:36 -0500

    Log NextResponse error in case of GET API Failures

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -6,6 +6,7 @@ export async function GET() {
     const projects = await fetchGitHubProjects();
     return NextResponse.json(projects);
   } catch (error) {
+    console.error('Failed to fetch projects:', error);
     return NextResponse.json({ error: 'Failed to fetch projects.' }, { status: 500 });
   }
 }
