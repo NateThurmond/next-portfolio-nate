e7df11b96471c6cb5ccb172450f8755c9c4944a0
NateThurmond <nathanthurmond@yahoo.com>
2025-03-27 23:47:11 -0500

    Add tailwind merge and optional class override to Int. Heading Comp

--- app/components/InteractiveHeading.tsx
+++ app/components/InteractiveHeading.tsx
@@ -2,17 +2,27 @@
 
 import "../styles/animations.css";
 import Link from "next/link";
+import { twMerge } from 'tailwind-merge';
 
 // Define the expected props
 type InteractiveHeadingProps = {
     headingText?: string; // Optional string prop
     onHeadingClick?: (event: React.MouseEvent<HTMLHeadingElement>) => void; // Function prop (optional)
     withLink?: string; // Optional boolean prop
+    classAdditional?: string; // Optional addl styling for h1
 };
 
-export default function InteractiveHeading({ headingText, onHeadingClick, withLink }: InteractiveHeadingProps) {
-  const Header = <h1 className="hover-drop cursor-pointer [text-shadow:1px_1px_0px_white] hover:tracking-wide"
-      onClick={onHeadingClick}>{headingText || ''}</h1>;
+export default function InteractiveHeading({
+  headingText,
+  onHeadingClick,
+  withLink,
+  classAdditional = ''
+}: InteractiveHeadingProps) {
 
-  return withLink ? <Link href={withLink}>{Header}</Link> : Header;
+  const baseHeaderClass = "hover-drop cursor-pointer [text-shadow:1px_1px_0px_white] hover:tracking-wide";
+
+  const Header = <h1 className={twMerge(baseHeaderClass, classAdditional)}
+    onClick={onHeadingClick}>{headingText || ''}</h1>;
+
+  return withLink ? <Link href={withLink} target="_blank">{Header}</Link> : Header;
 }
